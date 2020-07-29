5e8ea1e8ed3ea7da68c01a0cdca87c4f775b26ff
Simeon Anthony <simeonanthony556@gmail.com>
2020-07-29 14:48:20 -0500

    Initial Commit

---
+++ covid19tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid19tracker
+//
+//  Created by Student on 7/29/20.
+//  Copyright © 2020 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
